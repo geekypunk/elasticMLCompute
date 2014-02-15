019edf12af312ad48af6977e9a5f6617cdf23b67
Sasank Krishna Talasila <sasanktk@gmail.com>
2014-02-15 03:07:15 -0500

    Dataset Management UI

---
+++ src/com/cs5412/utils/HTTPConstants.java
@@ -0,0 +1,7 @@
+package com.cs5412.utils;
+
+public class HTTPConstants {
+	public static final String GET_DATASETS = "getDatasets";
+	public static final String DELETE_DATASET = "delfile";
+	public static final String DS_UI_PARAM_NAME = "dataset";
+}
