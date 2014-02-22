1b8275d366991f392a62fedbfd7c71e03c0de8f3
Sasank Krishna Talasila <sasanktk@gmail.com>
2014-02-22 01:50:51 -0500

    New JS

---
+++ WebContent/js/svm.charts.js
@@ -0,0 +1,120 @@
+var SVMCharts = function () {
+
+    return {
+        displayChart: function (data) {
+
+            if (!jQuery.plot) {
+                return;
+            }
+			/*
+			function multiply100(data){
+				var masterArray = [];
+				var dataPoint;
+				
+				$.each(data, function( index, value ) {
+					dataPoint = [];
+					$.each(value, function( index, value2 ) {
+						dataPoint.push()
+					}
+				}
+			
+			}*/
+			data = JSON.parse(data);
+			var allSeries=[];
+			var seriesObj;
+			$.each(data, function( index, value ) {
+				seriesObj={}
+				seriesObj.label = "C"+index;
+				seriesObj.data = value;
+				allSeries.push(seriesObj);
+			});
+           
+            	
+            /* Interactive Chart */
+            function chart2() {
+                
+                var plot = $.plot($("#chart_2"), allSeries, {
+                        series: {
+                            lines: {
+                                show: true,
+                                lineWidth: 1,
+                                fill: true,
+                                fillColor: {
+                                    colors: [{
+                                            opacity: 0.05
+                                        }, {
+                                            opacity: 0.01
+                                        }
+                                    ]
+                                }
+                            },
+                            points: {
+                                show: true
+                            },
+                            shadowSize: 2
+                        },
+                        grid: {
+                            hoverable: true,
+                            clickable: true,
+                            tickColor: "#eee",
+                            borderWidth: 0
+                        },
+                        colors: ["#DB5E8C", "#F0AD4E", "#5E87B0"],
+                        xaxis: {
+							ticks:7,
+                            min: 0.01,
+                            max: 10
+                        },
+                        yaxis: {
+                            ticks:7,
+							min: 0.6,
+                            max: 0.8
+                            
+                        }
+                    });
+
+
+                function showTooltip(x, y, contents) {
+                    $('<div id="tooltip">' + contents + '</div>').css({
+                            position: 'absolute',
+                            display: 'none',
+                            top: y + 5,
+                            left: x + 15,
+                            border: '1px solid #333',
+                            padding: '4px',
+                            color: '#fff',
+                            'border-radius': '3px',
+                            'background-color': '#333',
+                            opacity: 0.80
+                        }).appendTo("body").fadeIn(200);
+                }
+
+                var previousPoint = null;
+                $("#chart_2").bind("plothover", function (event, pos, item) {
+                    $("#x").text(pos.x.toFixed(2));
+                    $("#y").text(pos.y.toFixed(2));
+
+                    if (item) {
+                        if (previousPoint != item.dataIndex) {
+                            previousPoint = item.dataIndex;
+
+                            $("#tooltip").remove();
+                            var x = (item.datapoint[0]/100).toFixed(5),
+                                y = item.datapoint[1].toFixed(5);
+
+                            showTooltip(item.pageX, item.pageY, item.series.label + " of " + x + " = " + y);
+                        }
+                    } else {
+                        $("#tooltip").remove();
+                        previousPoint = null;
+                    }
+                });
+            }
+   
+           
+            chart2();
+			
+        },
+    };
+
+}();
---
+++ WebContent/js/svm.js
@@ -0,0 +1,75 @@
+
+		$(function(){
+			
+			$.getJSON("http://localhost:8080/elasticMLCompute/ml/svm/getTrainingDataSets",{id: $(this).val(), ajax: 'true'}, function(j){
+		 		var $subType = $("#e1");
+				$subType.empty();
+				$.each(j, function () {
+					$subType.append($('<option></option>').attr("value", this.optionValue).text(this.optionDisplay));
+				});
+			});
+			$.getJSON("http://localhost:8080/elasticMLCompute/ml/svm/getTestDataSets",{id: $(this).val(), ajax: 'true'}, function(j){
+		 		var $subType = $("#e2");
+				$subType.empty();
+				$.each(j, function () {
+					$subType.append($('<option></option>').attr("value", this.optionValue).text(this.optionDisplay));
+				});
+			});
+			
+		
+		})
+	
+		$("#svmForm").submit(function(event) {
+
+			  /* stop form from submitting normally */
+			event.preventDefault();
+
+			var a=$('#btn-load-complete');
+			a.button("loading");
+			var trainingDataset = $('#e1').val();
+			if(trainingDataset.trim().length > 0 && trainingDataset.trim().indexOf(".train")!=-1){	
+				$.ajax({
+					type: "POST",
+					data:{ 
+						trainingDataset: trainingDataset, 
+						testDataset: $('#e2').val() 
+					},
+					dataType: "text",
+					url: "http://localhost:8080/elasticMLCompute/ml/svm/runService",
+					success: function (d) {
+						a.button("complete");
+					},
+					error: function (d) {
+		
+					},
+					complete: function (d) {
+						$('#btn-load-complete').button("complete");
+						
+						
+						
+					}
+				});
+			}else{
+				alert("Choose a training dataset!");
+				$('#btn-load-complete').button("reset");
+			}
+		
+		});
+			
+		$("#btn-chart").on("click",function(){
+			$('#svmChart').show();
+			var a=$(this);
+			a.button("loading");
+			setTimeout(function() { 
+				$.ajax({
+					type: "GET",
+					dataType: "text",
+					url: "http://localhost:8080/elasticMLCompute/ml/svm/getReport/SVM.train-SVM.test.chart",
+					success: function (d) {
+						a.button("complete");
+						SVMCharts.displayChart(d);
+					}
+				})
+				},500)
+		});
+		
