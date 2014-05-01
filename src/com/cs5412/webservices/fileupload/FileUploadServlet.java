fe341df6d38b9cbcf372eb6032ef24ba03af47e2
Pritam <pbp36@cornell.edu>
2014-05-01 02:57:38 -0400

    http changes

--- src/com/cs5412/webservices/fileupload/FileUploadServlet.java
+++ src/com/cs5412/webservices/fileupload/FileUploadServlet.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
